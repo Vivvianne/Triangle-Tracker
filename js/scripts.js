f5ea26729bde64a8c5d5ed9117faf89cb68a0b1b
Vivvianne Kimani <ndiahav@gmail.com>
2019-04-25 21:55:35 +0300

    define the parameters and added the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-var triangle = function(Length1, length2, Length3)
+
+function triangleTracker(params) {
+    var length1 = parseInt(document.getElementById("length1").value);
+    var length2 = parseInt(document.getElementById("length2").value);
+    var length3 = parseInt(document.getElementById("length3").value);
+
+    var trianglePerimeter = [length1 + length2 + length3];
+}
